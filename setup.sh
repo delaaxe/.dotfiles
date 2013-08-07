0c2e288ebdffc22ad506587ede08b272c7c9d2d2
delaaxe <mau5design@live.fr>
2013-08-07 09:01:13 -0700

    edit install script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
-for file in .{bash_profile,gitignore,gitconfig,vim,vimrc,inputrc}; do
-  #ln -s ~/.dotfiles/$file ~/$file 
+for file in .{gitignore,gitconfig,vim,vimrc,inputrc}; do
   if [ -e ~/$file ]; then
-    echo "~/$file: already exists, not linked."
+    echo "~/$file: not linked, already exists."
   else
+    ln -s ~/.dotfiles/$file ~/$file 
     echo "~/$file: linked."
   fi
 done
 unset file
+echo ". ~/.dotfiles/.local.bash" >> ~/.bashrc
+echo ".local.bash: linked."
